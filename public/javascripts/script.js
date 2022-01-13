2ccc385c6c6c73fcced420c78b2a9f60aaf76ee9
JOSH Haywood <Joshhaywood025@gmail.com>
2022-01-13 13:09:49 +0000

    refactor: Altered 'ToggleNavLinks' to use 'getElementByClassName' in 'script.js' fix: Fixed bug where ToggleNavLinks would take two clicks on first load in 'script.js' style: Removed ID tags and href link on 'nav-toggle' from 'layout.hbs' fix: Removed background colour from links on hover as this wasn't aesthetically pleasing in 'style.css' feat: Added cursor pointer on hover over 'nav-toggle' in 'style.css'

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,6 +1,7 @@
 function ToggleNavLinks() { //Function to toggle navigation links display
-    var navLink = document.getElementById("navLinks"); //Gets ID element
-    if (navLink.style.display === "none") { //If currently not displayed
+    var navLink = document.getElementsByClassName("nav-links")[0]; //Gets first instance of class name
+    
+    if (window.getComputedStyle(navLink).display === "none") { //If currently not displayed, computed style is used to ensure element is set to none
         navLink.style.display = "flex"; //Set display to flex
     } 
     else {
