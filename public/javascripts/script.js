02d30d2e60cf621e86b673188d1efad26034e37f
JOSH Haywood <Joshhaywood025@gmail.com>
2022-01-14 16:41:04 +0000

    feat: Re-added 'stock.png'. feat: Added basic layout of hero section in 'index.hbs' and styled in 'style.css' and added scroll event for navigation bar in 'script.js' refactor: Made the navigation bar fixed instead of relative in 'style.css'.

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -8,3 +8,16 @@ function ToggleNavLinks() { //Function to toggle navigation links display
         navLink.style.display = "none"; //Else leave display as none
     }
 }
+
+window.addEventListener('scroll', function() {; //Event listener for mouse scroll
+
+    var navBar = document.getElementsByClassName("navbar")[0]; //Gets first instance of class name
+
+    if(scrollY >= 850){ //If user scroll value is greater than 850 (The area the hero takes up)
+        navBar.style.background = "#181a1d"; //Sets background colour to secondary
+    }
+
+    if (scrollY < 850) { //If user scroll value is less than 850 (User has gone back to the hero section)
+        navBar.style.background = "none"; //Sets background colour back to none
+    }   
+});
