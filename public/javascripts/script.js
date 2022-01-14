08cd1ec14d8654e5e2a46c8e2c81943c1fcb54be
JOSH Haywood <Joshhaywood025@gmail.com>
2022-01-14 20:30:22 +0000

    fix: Fixed bug where only navlinks would have a background colour instead of the entire navbar in 'script.js'. del: Removed flex-shrink from hero image in 'style.css'.

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,11 +1,14 @@
 function ToggleNavLinks() { //Function to toggle navigation links display
     var navLink = document.getElementsByClassName("nav-links")[0]; //Gets first instance of class name
+    var navBar = document.getElementsByClassName("navbar")[0]; //Gets first instance of class name
     
     if (window.getComputedStyle(navLink).display === "none") { //If currently not displayed, computed style is used to ensure element is set to none
         navLink.style.display = "flex"; //Set display to flex
+        navBar.style.background = "#181a1d"; //Sets background colour to secondary 
     } 
     else {
         navLink.style.display = "none"; //Else leave display as none
+        navBar.style.background = "none"; //Sets background back to none
     }
 }
 
