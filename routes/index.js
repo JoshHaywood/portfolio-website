16f7d6ffaf0693a1685c79f7d7cbbecc402ee600
JOSH Haywood <Joshhaywood025@gmail.com>
2022-01-27 14:58:44 +0000

    Feat(error.hbs, style.css) Added content to error page and styling. feat(index.js, hbs) Added basic page routing. Refactor(CSS) Altered grammatical errors

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET Home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Josh Haywood | Portfolio' });
+});
+
+/* GET Projects page */
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Projects' });
+});
+
+/* GET Contact page */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact' });
 });
 
 module.exports = router;
