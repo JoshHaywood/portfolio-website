5b617fe44374fc34e27b3e0954c09b8fbbe7d277
JOSH Haywood <jh248828@falmouth.ac.uk>
2022-06-16 21:15:03 +0100

    refactor(hero) Removed view more button. Altered styling elements.

--- src/components/index/hero.js
+++ src/components/index/hero.js
@@ -12,19 +12,19 @@ export default function Hero() {
     return (
         <div className="h-screen relative flex flex-col animate-fade-in-up">
             {/* Background video */}
-            <video autoPlay muted loop className="w-full h-screen object-cover brightness-75 blur-sm"> {/* Object cover allows videos aspect ratio to be overridden */}
+            <video autoPlay muted loop className="w-full h-screen object-cover brightness-50 blur-sm"> {/* Object cover allows videos aspect ratio to be overridden */}
                 {/* Attribution:https://pixabay.com/videos/circuit-electronics-digital-27725/ by motionstock */}
                 <source src="../videos/background.mp4" type="video/mp4"/>
                 Your browser does not support videos
             </video>
 
-            <div className="absolute lg:left-[20%] h-screen flex flex-col justify-center mx-10 lg:mx-auto z-10 space-y-6"> {/* Prevents media icons from being flexed in wrong direction */}
+            <div className="absolute lg:left-[20%] h-screen flex flex-col justify-center mx-10 lg:mx-auto z-10 space-y-6 leading-10 sm:leading-none"> {/* Prevents media icons from being flexed in wrong direction */}
                 {/* Text */}
                 <div className="space-y-3 md:space-y-6"> {/* Prevents text being effected by flex */}
-                    <h3>Hi, my name is</h3>
+                    <h3 className="text-primary">Hi, my name is</h3>
                     <h1 className="sm:text-5xl md:text-6xl font-semibold">Josh Haywood</h1>
-                    <h1 className="sm:text-5xl md:text-6xl font-semibold">An aspiring web developer</h1>
-                    <p className="text-xl sm:w-[80%] md:w-[60%] lg:w-[48%]">
+                    <h1 className="sm:text-5xl md:text-6xl font-semibold text-gray-400">An aspiring web developer</h1>
+                    <p className="text-xl sm:w-[80%] md:w-[60%] lg:w-[48%] text-gray-400">
                         Im currently a Web Development student at <span className='font-semibold text-primary'>Falmouth University</span> in my final year of a BSC.
                         Currently im focused on specializing in front-end development.
                     </p>
@@ -47,7 +47,7 @@ export default function Hero() {
                         View my Github
                         </Button>                            
                     
-                        <a href="https://github.com/El-Goblino/" className="h-4 w-4 -ml-[26px] mt-[10px] z-10">
+                        <a href="https://github.com/El-Goblino/" className="h-4 w-4 -ml-[24px] mt-[9px] z-10">
                             <svg alt="Github logo" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="github" classname="svg-inline--fa fa-github" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"> {/*! Font Awesome Pro 6.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. */} 
                                 <path fill="currentColor" d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"/>
                             </svg>
@@ -76,14 +76,6 @@ export default function Hero() {
                         </a>
                     </div>
                 </div>
-            </div>
-
-            {/* Hero dropdown */}
-            <div id="pageDown" className="flex flex-col items-center -mt-16 z-10 w-fit mx-auto">
-                <p>View more</p>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" className="fill-white h-8 w-8 transition duration-300 ease-in-out"> {/* Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. */}
-                    <path d="M192 384c-8.188 0-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L192 306.8l137.4-137.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-160 160C208.4 380.9 200.2 384 192 384z"/>
-                </svg>
             </div>
         </div>
     );
