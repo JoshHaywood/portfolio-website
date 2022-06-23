b578e5ce551572b2da834814a4105ab5565cc001
JOSH Haywood <jh248828@falmouth.ac.uk>
2022-06-23 22:29:56 +0100

    fix(hero) Altered buttom dimesions. refactor(message) Altered styling to match index.

--- src/components/message.js
+++ src/components/message.js
@@ -6,26 +6,26 @@ export default function Message(props) {
     const navigate = useNavigate();
 
     return (
-        <div className="flex flex-col text-center justify-center h-screen space-y-8 pb-20">
-            <h1 className="text-white text-6xl">{props.heading}</h1>
+        <div className="flex flex-col text-center justify-center h-screen space-y-10">
+            <h1 className="text-white text-6xl font-semibold">{props.heading}</h1>
 
             <h5>{props.message}</h5>
 
             <Button
-                    onClick={() => navigate(-1)}
-                    sx={{
-                            backgroundColor: '#597de0',
-                            color: 'white',
-                            width: '10%',
-                            marginLeft: 'auto',
-                            marginRight: 'auto',
+                onClick={() => navigate(-1)}
 
-                            ':hover': {
-                                bgcolor: 'rgba(76, 107, 193, 0.75)', 
-                            },
-                        }}
-                >
-                Go Back
+                sx={{
+                        border: '2px solid #4c6bc1',
+                        color: 'white',
+                        marginLeft: 'auto',
+                        marginRight: 'auto',
+
+                        ':hover': {
+                            bgcolor: 'rgba(76, 107, 193, 0.3)',
+                        },
+                    }}
+            >
+            Go Back
             </Button>
         </div>
     );
