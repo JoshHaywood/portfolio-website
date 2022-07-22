bab1cb0a868de4c9e42cf349d9ae92e21be972e1
JOSH Haywood <jh248828@falmouth.ac.uk>
2022-07-22 20:50:24 +0100

    refactor(index animations) Altered animation listeners to use GSAP animations.

--- src/components/index/about.js
+++ src/components/index/about.js
@@ -1,47 +1,40 @@
 import  * as React from 'react';
+import { useEffect } from 'react';
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-const items = [
-    {text: "HTML - 3 years"},
-    {text: "CSS - 3 years"},
-    {text: "Client side JavaScript - 6 months"},
-    {text: "Handlebars - 6 months"},
-    {text: "Node.js  - 6 months"},
-    {text: "Express.js  - 6 months"},
-    {text: "C#.net and Unity C# - 18 months"},
-    {text: "C++ - 18 months"},
-    {text: "Unity - 18 months"},
-];
+gsap.registerPlugin(ScrollTrigger);
 
 export default function About() {
-    //Animation scroll event
-    window.addEventListener('scroll', function() {
-        let triggerElement = document.getElementById("aboutHeading"); //Element used as trigger
-        let bounding = triggerElement.getBoundingClientRect();
+    const items = [
+        {text: "HTML - 3 years"},
+        {text: "CSS - 3 years"},
+        {text: "Client side JavaScript - 6 months"},
+        {text: "Handlebars - 6 months"},
+        {text: "Node.js  - 6 months"},
+        {text: "Express.js  - 6 months"},
+        {text: "C#.net and Unity C# - 18 months"},
+        {text: "C++ - 18 months"},
+        {text: "Unity - 18 months"},
+    ];
 
-        let targetedElement = document.getElementById("section"); //Element animation is applied too
-
-        // Gets boundaries using getBoundingClientRect
-        // If in viewport
-        if (bounding.top >= 0 &&
-            bounding.left >= 0 &&
-            //Compares to element dimensions
-            bounding.right <= (window.innerWidth || document.documentElement.clientWidth) &&
-            bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)) {
-
-            targetedElement.classList.add('fade-in-up');
-        };
+    //Animation
+    useEffect(() => {
+          gsap.from('#aboutSection', {duration: 1, y: '10%', opacity: '0', ease: 'sine', delay: '0.5', scrollTrigger: {
+            trigger: "#aboutSection",
+        }});
     });
 
     return (
         <div className="space-y-12">
             {/* Heading */}
             <div className="space-y-3 mx-auto">
-                <h2 id="aboutHeading" className="font-semibold text-center">About me</h2>
+                <h2 className="font-semibold text-center">About me</h2>
                 <div className="w-24 h-1 bg-primary mx-auto"></div>
             </div>
 
             {/* Text */}
-            <div id="section" className="flex flex-col md:flex-row md:justify-evenly items-center md:items-start md:space-x-10 space-y-[4rem] md:space-y-0 px-5 md:px-10 lg:px-0 pb-10 sm:pb-0"> 
+            <div id="aboutSection" className="flex flex-col md:flex-row md:justify-evenly items-center md:items-start md:space-x-10 space-y-[4rem] md:space-y-0 px-5 md:px-10 lg:px-0 pb-10 sm:pb-0"> 
                 <div className="flex flex-col space-y-4 w-100% md:w-3/5 leading-7">
                     <p>
                         I’m a student from Devon, England. 
--- src/components/index/projects.js
+++ src/components/index/projects.js
@@ -1,10 +1,14 @@
 import * as React from 'react';
+import { useEffect } from 'react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { Link } from 'react-router-dom';
+
+gsap.registerPlugin(ScrollTrigger);
 
 export default function Projects() {
     const items = [
         {
-            id: "projectOne",
             imageSrc: "../Images/portfolio-thumbnail.jpg",
             tagline: "Personal Site",
             heading: "Portfolio Website",
@@ -14,7 +18,6 @@ export default function Projects() {
         },
     
         {
-            id: "projectTwo",
             imageSrc: "../Images/one-messaging-thumbnail.jpg",
             tagline: "Messaging Project",
             heading: "One messaging project",
@@ -23,38 +26,36 @@ export default function Projects() {
             list: "Handlebars ExpressJS SocketIO SQLite",
         },
     ];  
-    
-    const technologies = [];
-    const listItems = technologies.map((technologies) =>
-      <li className="text-gray-400">{technologies}</li>
-    );
 
-    //Animation scroll event
-    window.addEventListener('scroll', function() {
-        for (let i = 0; i < items.length; i++) { //Ensures all mapped elements have animation applied
-            let triggerElement = document.getElementById("projectsHeading"); //Element used as trigger
-            let bounding = triggerElement.getBoundingClientRect();
+    //Animation
+    useEffect(() => {
+        gsap.from('#projectOneImg', {duration: 1, y: '10%', opacity: '0', ease: 'sine', delay: '0.5', scrollTrigger: {
+            trigger: "#projectOneImg",
+        }});
+        gsap.from('#projectOneCard', {duration: 1, x: '50%', opacity: '0', ease: 'power4', delay: '1', scrollTrigger: {
+            trigger: "#projectOneImg",
+        }});
+        gsap.from('#projectOneList', {duration: 1, opacity: '0', ease: 'sine', delay: '2', scrollTrigger: {
+            trigger: "#projectOneImg",
+        }});
 
-            let targetedElement = document.getElementById(items[i].id); //Element animation is applied too
 
-            // Gets boundaries using getBoundingClientRect
-            // If in viewport
-            if (bounding.top >= 0 &&
-                bounding.left >= 0 &&
-                //Compares to element dimensions
-                bounding.right <= (window.innerWidth || document.documentElement.clientWidth) &&
-                bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)) {
-
-                targetedElement.classList.add('fade-in-up');
-            };
-        };
+        gsap.from('#projectTwoImg', {duration: 1, y: '10%', opacity: '0', ease: 'sine', delay: '0.5', scrollTrigger: {
+            trigger: "#projectTwoImg",
+        }});
+        gsap.from('#projectTwoCard', {duration: 1, x: '-50%', opacity: '0', ease: 'power4', delay: '1', scrollTrigger: {
+            trigger: "#projectTwoImg",
+        }});
+        gsap.from('#projectTwoList', {duration: 1, opacity: '0', ease: 'sine', delay: '2', scrollTrigger: {
+            trigger: "#projectTwoImg",
+        }});
     });
 
     return (
         <div className="md:w-4/5 xl:w-3/5 mx-auto pt-20 md:pt-16 xl:pt-36 pb-12 md:pb-20 xl:pb-40"> 
             {/* Heading */}
             <div className="space-y-3 mx-auto"> 
-                <h2 id="projectsHeading" className="font-semibold text-center">Featured Projects</h2>
+                <h2 className="font-semibold text-center">Featured Projects</h2>
                 <div className="w-48 h-1 bg-primary mx-auto"></div>
             </div>
 
@@ -63,9 +64,9 @@ export default function Projects() {
                 {items.map((items, index) => 
                     index == 0 && (
                         <Link to={items.link}>
-                            <div id={items.id} className="mt-12 md:space-x-10 flex md:flex-row items-center text-left md:text-right"> {/* Objects mapped ID used so all mapped elements have scroll listener applied */}
+                            <div className="mt-12 md:space-x-10 flex md:flex-row items-center text-left md:text-right"> {/* Objects mapped ID used so all mapped elements have scroll listener applied */}
                                 {/* Image */}
-                                    <div className="relative w-full md:w-3/5 overflow-hidden"> {/* Container for overlaying child elements */}
+                                    <div id="projectOneImg" className="relative w-full md:w-3/5 overflow-hidden"> {/* Container for overlaying child elements */}
                                         <div className="hover:animate-pop-out">  {/* Animation added as child so parent elements scale isnt effected */}
                                             <div className="absolute bg-primary w-full h-full z-10 brightness-[20%] md:brightness-100 opacity-90 md:opacity-30 md:hover:opacity-0 transition duration-300 ease-in-out"></div>
                                             <img className="rounded-sm h-[35vh] md:h-full w-min" alt="Image of feature project" src={items.imageSrc}></img>
@@ -74,15 +75,17 @@ export default function Projects() {
 
                                     {/* Text card */}
                                     <div className="flex flex-col items-start md:items-end z-20 sm:pr-20 md:pr-0 md:w-2/5 mx-[0.7rem] sm:mx-10 pb-5 absolute md:static">
-                                        <h6 className="text-primary">{items.tagline}</h6>
-                                        <h4 className="mb-5 font-semibold text-primary-text">{items.heading}</h4>
+                                        <div id="projectOneCard"> {/* Container for animation trigger */}
+                                            <h6 className="text-primary">{items.tagline}</h6>
+                                            <h4 className="mb-5 font-semibold text-primary-text">{items.heading}</h4>
 
-                                        <p className="bg-none md:bg-senary md:pl-10 pr-5 sm:pt-5 pb-5 md:-ml-32 shadow-md hover:shadow-lg rounded-sm text-gray-300">
-                                            {items.text}
-                                        </p>
+                                            <p className="bg-none md:bg-senary md:pl-10 pr-5 sm:pt-5 pb-5 md:-ml-32 shadow-md hover:shadow-lg rounded-sm text-gray-300">
+                                                {items.text}
+                                            </p>
+                                        </div>
 
                                         {/* Stack list */}
-                                        <ul id="projectList" className="flex flex-row sm:pt-5">
+                                        <ul id="projectOneList" className="flex flex-row sm:pt-5">
                                             <li>{items.list}</li>
                                         </ul>
                                     </div>
@@ -95,24 +98,26 @@ export default function Projects() {
                 {items.map((items, index) => 
                     index == 1 && (
                         <Link to={items.link}>
-                            <div id={items.id} className="mt-8 lg:mt-20 md:space-x-10 flex md:flex-row items-center"> {/* Objects mapped ID used so all mapped elements have scroll listener applied */}
+                            <div className="mt-8 lg:mt-20 md:space-x-10 flex md:flex-row items-center"> {/* Objects mapped ID used so all mapped elements have scroll listener applied */}
                                 {/* Text card */}
                                 <div className="flex flex-col items-start z-20 sm:pr-20 md:pr-0 md:w-2/5 mx-[0.7rem] sm:mx-10 md:mx-0 pb-5 absolute md:static">
-                                    <h6 className="text-primary">{items.tagline}</h6>
-                                    <h4 className="mb-5 font-semibold text-primary-text">{items.heading}</h4>
+                                    <div id="projectTwoCard"> {/* Container for animation trigger */}
+                                        <h6 className="text-primary">{items.tagline}</h6>
+                                        <h4 className="mb-5 font-semibold text-primary-text">{items.heading}</h4>
 
-                                    <p className="bg-none md:bg-senary text-left md:pl-5 pr-10 sm:pt-5 pb-5 md:-mr-32 shadow-md hover:shadow-lg rounded-sm text-gray-300">
-                                        {items.text}
-                                    </p>
+                                        <p className="bg-none md:bg-senary text-left md:pl-5 pr-10 sm:pt-5 pb-5 md:-mr-32 shadow-md hover:shadow-lg rounded-sm text-gray-300">
+                                            {items.text}
+                                        </p>
+                                    </div>
 
                                     {/* Stack list */}
-                                    <ul id="projectList" className="flex flex-row sm:pt-5">
+                                    <ul id="projectTwoList" className="flex flex-row sm:pt-5">
                                         <li>{items.list}</li>
                                     </ul>
                                 </div>
 
                                 {/* Image */}
-                                <div className="relative w-full md:w-3/5 overflow-hidden"> {/* Container for overlaying child elements */}
+                                <div id="projectTwoImg" className="relative w-full md:w-3/5 overflow-hidden"> {/* Container for overlaying child elements */}
                                     <div className="hover:animate-pop-out">  {/* Animation added as child so parent elements scale isnt effected */}
                                         <div className="absolute bg-primary w-full h-full z-10 brightness-[20%] md:brightness-100 opacity-90 md:opacity-30 md:hover:opacity-0 transition duration-300 ease-in-out"></div>
                                         <img className="rounded-sm h-[35vh] md:h-full w-min" alt="Image of feature project" src={items.imageSrc}></img>
