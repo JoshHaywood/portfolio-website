7f8210878a2915274617fd2c16f13bfa17fb3863
JOSH Haywood <jh248828@falmouth.ac.uk>
2022-07-28 21:28:15 +0100

    chore: Reverted previous IOS commits.

--- src/components/index/hero.js
+++ src/components/index/hero.js
@@ -18,7 +18,7 @@ export default function Hero() {
     return (
         <div class="video-responsive" className="h-screen relative flex flex-col w-full -z-10">
             {/* Background video */}
-            <video class="video" autoPlay muted loop playsInline className="brightness-50 blur-sm"> {/* Object cover allows videos aspect ratio to be overridden */}
+            <video autoPlay muted loop className="w-full h-screen object-cover brightness-50 blur-sm"> {/* Object cover allows videos aspect ratio to be overridden */}
                 {/* Attribution:https://pixabay.com/videos/circuit-electronics-digital-27725/ by motionstock */}
                 <source src="../videos/background.mp4" type="video/mp4"/>
                 Your browser does not support videos
