50e1c68e0cc6a6ee04de91a1a62e98d17d8782c4
JOSH Haywood <jh248828@falmouth.ac.uk>
2022-08-16 01:42:44 +0100

    feat: Added CV view button. fix: Fixed animation not working on tablet.

--- src/components/header.js
+++ src/components/header.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
 import { Squash as Hamburger } from 'hamburger-react';
+import Button from '@mui/material/Button';
+
+import Pdf from '../files/cv.pdf';
 
 export default function Header() {
   const links = [
@@ -29,7 +32,7 @@ export default function Header() {
   window.addEventListener('scroll', handleScroll);
 
   //Animation
-  const isMobile = window.innerWidth < 1024; 
+  const isMobile = window.innerWidth < 768; 
   let containerVariants = {};
   let listVariants = {};
 
@@ -40,7 +43,7 @@ export default function Header() {
       hidden: {},
       visible: {
         transition: {
-          delayChildren: 2.9,
+          delayChildren: 3.4,
           staggerChildren: 0.2,
         }
       }
@@ -66,7 +69,7 @@ export default function Header() {
         isOpen || scrolled
         ? `bg-tertiary`
         : `bg-none`
-      } w-full h-[75px] fixed top-0 sm:px-6 flex justify-between items-center z-50 backdrop-blur`}
+      } w-full h-[75px] fixed top-0 sm:px-5 flex justify-between items-center z-50 backdrop-blur`}
       initial= {{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 0.1, delay: 2.8 }}    
@@ -76,7 +79,7 @@ export default function Header() {
         <div className="bg-[url('../public/Images/logo.png')] w-[260px] h-[50px] hover:bg-[url('../public/Images/logo-hover.png')] bg-[length:260px] bg-no-repeat" alt="logo"></div>
       </Link>
 
-      <div className="flex"> {/* Container for links and menu */}
+      <div className="flex items-center mb-1"> {/* Container for links and menu */}
         {/* Hamburger */}
         <div className="md:hidden mb-0.5 z-10"> {/* Had to use parent element as Tailwind doesn't effect custom React components */}
           <Hamburger
@@ -104,12 +107,12 @@ export default function Header() {
           animate="visible"
           className={`${
             isOpen
-              ?  `h-screen w-3/5 sm:w-1/2 overflow-y-scroll flex flex-col lg:flex-row items-center absolute top-0 right-0 pt-40 bg-tertiary shadow-md shadow-primary`
+              ?  `h-screen w-3/5 sm:w-1/2 overflow-y-scroll flex flex-col lg:flex-row absolute top-0 right-0 pt-40 pl-5 bg-tertiary shadow-md shadow-primary`
               : `hidden`
-          } md:flex flex-row`}
+          } md:flex flex-row items-center`}
         >
           {links.map((link) => (
-            <motion.li variants={listVariants} id={link.id} className="m-5 lg:my-0 lg:mx-5 leading-10">
+            <motion.li variants={listVariants} id={link.id} className="mx-3 my-5 md:my-0 md:order-2">
               <Link 
                 className="text-2xl font-medium text-white hover:text-primary transition duration-300 ease-in-out"
                 to={link.path} 
@@ -119,6 +122,31 @@ export default function Header() {
               </Link>
             </motion.li>
           ))}
+
+          {/* Container for framer animation */}
+          <motion.div 
+            initial= {{ opacity: 0, y: '-80%' }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, delay: 2.9, ease: 'easeInOut' }}
+            className="md:mr-3"
+          >
+            <Button
+                href={Pdf} 
+                target="_blank"
+                sx={{
+                        border: '2px solid #4c6bc1',
+                        color: 'white',
+                        bgcolor: 'rgba(76, 107, 193, 0.3)',
+                        margin: '1.25rem 0',
+
+                        ':hover': {
+                          bgcolor: 'none',
+                        },
+                    }}
+            >
+            View my CV
+            </Button>
+          </motion.div>
         </motion.ul>
       </div>
     </motion.nav>
