5b2bef0d19362413f2244a97f7c4a36b661c30f6
JOSH Haywood <jh248828@falmouth.ac.uk>
2022-12-18 22:28:28 +0000

    refactor(project) Separated cards into individual functions. refactor(about) Improved .map maintainability.  refactor(hero) Removed icons and altered styling

--- src/components/index/about.js
+++ src/components/index/about.js
@@ -1,81 +1,90 @@
-import React from 'react';
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 
 export default function About() {
-    const items = [
-        {text: 'HTML'},
-        {text: 'CSS'},
-        {text: 'JavaScript'},
-        {text: 'Tailwind'},
-        {text: 'Node.js'},
-        {text: 'React'},
-        {text: 'Material UI'},
-        {text: 'Git'},
-        {text: 'Heroku'},
-    ];
+  const columns = [
+    {
+      technologies: [
+        { label: 'HTML' },
+        { label: 'CSS' },
+        { label: 'JavaScript' },
+      ],
+    },
+    {
+      technologies: [
+        { label: 'Tailwind' },
+        { label: 'Node.js' },
+        { label: 'React' },
+      ],
+    },
+    {
+      technologies: [
+        { label: 'Material UI' },
+        { label: 'Git' },
+        { label: 'Heroku' },
+      ],
+    },
+  ];
 
-    return (
-        //Animation
-        <motion.div 
-            className="space-y-12" 
-            initial= {{ opacity: 0, y: '5%' }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1, delay: 0.5, ease: 'easeInOut' }}
-            viewport={{ once: true }}
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: "5%" }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 1, delay: 0.5, ease: "easeInOut" }}
+      viewport={{ once: true }}
+      class="space-y-12"
+    >
+      {/* Heading */}
+      <div class="space-y-3 mx-auto">
+        <h2 class="font-semibold text-4xl text-center">About Me</h2>
+        <div class="w-24 h-1 bg-primary mx-auto"></div>
+      </div>
+
+      {/* Background information */}
+      <div class="flex flex-col md:flex-row md:justify-evenly items-center md:items-start md:space-x-10 space-y-[4rem] md:space-y-0">
+        <div class="flex flex-col space-y-4 w-100% md:w-3/5 leading-7">
+          <p>
+            I’m a student from Devon, England currently studying a BSC in Web
+            Development. My interest for Web Development came from my
+            educational background, studying Information Technologies and later
+            Computer Software.
+          </p>
+
+          <p>
+            My main interests are in front-end, full stack or user interface
+            development and design. Recently I've been focused on learning
+            React, Sass, Azure including marketing myself using my portfolio
+            site and CV.
+          </p>
+
+          <p class="text-gray-400">Technologies I have been using recently</p>
+          <div class="grid grid-cols-3">
+            {columns.map((column, index) => (
+              <ul key={index}>
+                {column.technologies.map((item, index) => (
+                  <li
+                    key={index}
+                    class="list-disc marker:text-primary ml-4 text-sm"
+                  >
+                    {item.label}
+                  </li>
+                ))}
+              </ul>
+            ))}
+          </div>
+        </div>
+
+        {/* Image */}
+        <div
+          id="hero-image"
+          class="max-w-[200px] sm:max-w-[300px] border-2 border-primary rounded-md h-min relative mx-auto md:top-5 left-5"
         >
-            {/* Heading */}
-            <div className="space-y-3 mx-auto">
-                <h2 className="font-semibold text-4xl text-center">About Me</h2>
-                <div className="w-24 h-1 bg-primary mx-auto"></div>
-            </div>
-
-            {/* Text */}
-            <div className="flex flex-col md:flex-row md:justify-evenly items-center md:items-start md:space-x-10 space-y-[4rem] md:space-y-0"> 
-                <div className="flex flex-col space-y-4 w-100% md:w-3/5 leading-7">
-                    <p>
-                        I’m a student from Devon, England currently studying a BSC in Web Development.
-                        My interest for Web Development came from my educational background, studying Information Technologies and later Computer Software. 
-                    </p>
-
-                    <p>
-                        My main interests are in front-end, full stack or user interface development and design. 
-                        Recently I've been focused on learning React, Sass, Azure including marketing myself using my portfolio site and CV.
-                    </p>
-
-                    <span className="text-gray-400">Technologies I have been using recently</span>
-                    <div className="flex justify-between">
-                        <ul>
-                            {/* Maps first three objects */}
-                            {items.map((items, index) => 
-                                index < 3 && (
-                                    <li className="list-disc marker:text-primary ml-4 text-sm">{items.text}</li>
-                                )
-                            )}
-                        </ul>
-                        <ul>
-                            {/* Maps objects between third and sixth */}
-                            {items.map((items, index) => 
-                                index >= 3 && index < 6 && (
-                                    <li className="list-disc marker:text-primary ml-4 text-sm">{items.text}</li>
-                                )
-                            )}
-                        </ul>
-                        <ul>
-                            {/* Maps final three objects */}
-                            {items.map((items, index) => 
-                                index >= 6 && (
-                                    <li className="list-disc marker:text-primary ml-4 text-sm">{items.text}</li>
-                                )
-                            )}
-                        </ul>
-                    </div>
-                </div>
-
-                {/* Image */}
-                <div id="hero-image" className="max-w-[200px] sm:max-w-[300px] border-2 border-primary rounded-md h-min relative mx-auto md:top-5 left-5"> {/* Creates border behind image */}
-                    <img src="../Images/portrait.webp" alt="developer portrait" className="rounded-md relative bottom-5 right-5"></img>
-                </div>
-            </div>
-        </motion.div>
-    )
-}
+          <img
+            src="../Images/portrait.webp"
+            alt="developer portrait"
+            class="rounded-md relative bottom-5 right-5"
+          ></img>
+        </div>
+      </div>
+    </motion.div>
+  );
+};
--- src/components/index/projects.js
+++ src/components/index/projects.js
@@ -1,245 +1,246 @@
-import React from 'react';
 import { motion } from 'framer-motion';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 export default function Projects() {
-    const items = [
-        {
-            imageSrc: '../Images/portfolio-thumbnail.webp',
-            tagline: 'Personal Site',
-            heading: 'Portfolio Website',
-            text: `My website showcasing projects I've created as well as a hub for marketing and contact. Specifically, my first version of the portfolio site and how Ive adapted the project since then.`,
-            link: '/projects/portfolio',
-            list: 'Handlebars/React ExpressJS CSS/Tailwind',
-        },
-    
-        {
-            imageSrc: '../Images/one-messaging-thumbnail.webp',
-            tagline: 'Messaging Project',
-            heading: 'One messaging project',
-            text: 'A realtime messaging app I made in my second year of University. This was project portion of my research into the advancement web communication systems.',
-            link: '/projects/one-messaging',
-            list: 'Handlebars ExpressJS SocketIO SQLite',
-        },
-    ];  
+  const navigate = useNavigate();
 
-    //Animation
-    const isMobile = window.innerWidth < 768; 
-    let containerVariants = {};
-    let imageVariants = {};
-    let leftCardVariants = {};
-    let rightCardVariants = {};
-    let listVariants = {};
+  const isMobile = window.innerWidth < 768;
+  let containerVariants = {};
+  let imageVariants = {};
+  let leftCardVariants = {};
+  let rightCardVariants = {};
+  let listVariants = {};
 
-    let textVariants = {};
+  let textVariants = {};
 
-    //Prevents animations on mobile
-    if (!isMobile) {
-        //Animation Variants
-        //Parent container that holds properties
-        containerVariants = {
-            hidden: {},
-            visible: {},
-        };
-        
-        //Individual component animations
-        imageVariants = {
-            hidden: { opacity: 0, y: '3%' },
-            visible: { 
-                opacity: 1,
-                y: 0,
-
-                transition: {
-                    duration: 0.5,
-                    delay: 0.5,
-                    ease: 'easeInOut',
-                } 
-            }
-        };
-
-        leftCardVariants = {
-            hidden: { opacity: 0, x: '-10%' },
-            visible: {
-                opacity: 1, 
-                x: 0,
-
-                transition: {
-                    duration: 0.5,
-                    delay: 1.3,
-                    ease: 'easeInOut',
-                } 
-            }
-        };
-
-        rightCardVariants = {
-            hidden: { opacity: 0, x: '10%' },
-            visible: { 
-                opacity: 1,
-                x: 0,
-
-                transition: {
-                    duration: 0.5,
-                    delay: 1.3,
-                    ease: 'easeInOut',
-                } 
-            }
-        };
-
-        listVariants = {
-            hidden: { opacity: 0 },
-            visible: { 
-                opacity: 1,
-
-                transition: {
-                    duration: 0.5,
-                    delay: 1.8,
-                    ease: 'easeInOut',
-                } 
-            }
-        };
-    }
-
-    else {
-        imageVariants = {
-            hidden: { 
-                opacity: 0,
-                y: '5%'
-            },
-            visible: { 
-                opacity: 1,
-                y: 0,
-                transition: {
-                    duration: 1,
-                    delay: 0.5,
-                    ease: 'easeInOut',
-                }
-            },
-        };
-
-        leftCardVariants = {
-            hidden: { opacity: 0 },
-            visible: {
-                opacity: 1, 
-
-                transition: {
-                    duration: 0.5,
-                    delay: 1.3,
-                    ease: 'easeInOut',
-                } 
-            }
-        };
-
-        rightCardVariants = {
-            hidden: { opacity: 0 },
-            visible: { 
-                opacity: 1,
-
-                transition: {
-                    duration: 0.5,
-                    delay: 1.3,
-                    ease: 'easeInOut',
-                } 
-            }
-        };
-
-        listVariants = {
-            hidden: { opacity: 0 },
-            visible: { 
-                opacity: 1,
-
-                transition: {
-                    duration: 0.5,
-                    delay: 1.3,
-                    ease: 'easeInOut',
-                } 
-            }
-        };
+  // Prevents animations on mobile
+  if (!isMobile) {
+    //Parent container that holds properties
+    containerVariants = {
+      hidden: {},
+      visible: {},
     };
 
+    //Individual component animations
+    imageVariants = {
+      hidden: { opacity: 0, y: "3%" },
+      visible: {
+        opacity: 1,
+        y: 0,
+
+        transition: {
+          duration: 0.5,
+          delay: 0.5,
+          ease: "easeInOut",
+        },
+      },
+    };
+
+    leftCardVariants = {
+      hidden: { opacity: 0, x: "-10%" },
+      visible: {
+        opacity: 1,
+        x: 0,
+
+        transition: {
+          duration: 0.5,
+          delay: 1.3,
+          ease: "easeInOut",
+        },
+      },
+    };
+
+    rightCardVariants = {
+      hidden: { opacity: 0, x: "10%" },
+      visible: {
+        opacity: 1,
+        x: 0,
+
+        transition: {
+          duration: 0.5,
+          delay: 1.3,
+          ease: "easeInOut",
+        },
+      },
+    };
+
+    listVariants = {
+      hidden: { opacity: 0 },
+      visible: {
+        opacity: 1,
+
+        transition: {
+          duration: 0.5,
+          delay: 1.8,
+          ease: "easeInOut",
+        },
+      },
+    };
+  } else {
+    imageVariants = {
+      hidden: { opacity: 0, y: "5%"},
+      visible: {
+        opacity: 1,
+        y: 0,
+        transition: {
+          duration: 1,
+          delay: 0.5,
+          ease: "easeInOut",
+        },
+      },
+    };
+
+    leftCardVariants = {
+      hidden: { opacity: 0 },
+      visible: {
+        opacity: 1,
+
+        transition: {
+          duration: 0.5,
+          delay: 1.3,
+          ease: "easeInOut",
+        },
+      },
+    };
+
+    rightCardVariants = {
+      hidden: { opacity: 0 },
+      visible: {
+        opacity: 1,
+
+        transition: {
+          duration: 0.5,
+          delay: 1.3,
+          ease: "easeInOut",
+        },
+      },
+    };
+  };
+
+  // Right aligned card
+  function AlignedRightCard() {
     return (
-        <> 
-            {/* Heading */}
-            <div className="space-y-3 mx-auto"> 
-                <h2 className="font-semibold text-4xl text-center">Featured Projects</h2>
-                <div className="w-48 h-1 bg-primary mx-auto"></div>
+        <motion.div 
+          variants={containerVariants}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          onClick={() => navigate("/projects/portfolio")}
+          class="relative bg-[url('../public/Images/portfolio-thumbnail.webp')] md:bg-none bg-fit bg-no-repeat bg-center shadow-[inset_0_2000px_0_0_rgba(76,107,193,0.3)] md:shadow-none md:flex md:flex-row md:space-x-5 hover:cursor-pointer"
+        >
+          {/* Image */}
+          <motion.div
+            variants={imageVariants}
+            class="hidden md:block md:w-3/5 md:relative md:overflow-hidden"
+          >
+            <div class="hover:scale-105 transition duration-500 ease-in-out">
+              {/* Image overlay */}
+              <div class="absolute bg-primary w-full h-full z-10 opacity-30 md:hover:opacity-0 transition duration-500 ease-in-out"></div>
+
+              <img
+                src="../Images/portfolio-thumbnail.webp"
+                alt="Featured project"
+                class="rounded max-w-[600px]"
+              ></img>
             </div>
+          </motion.div>
 
-            {/* Project section */}
-            <>
-                {items.map((items, index) => 
-                    index == 0 && (
-                        //Project One
-                        <motion.div variants={containerVariants} initial="hidden" whileInView="visible" viewport={{ once: true }}> {/* Motions own container as not compatible with Link */}
-                            <Link to={items.link} >
-                                <div className="mt-12 md:space-x-10 flex md:flex-row items-center text-left md:text-right"> {/* Objects mapped ID used so all mapped elements have scroll listener applied */}
-                                    {/* Image */}
-                                        <motion.div variants={imageVariants} className="relative overflow-hidden"
-                                        > {/* Container for overlaying child elements */}
-                                            <div className="hover:animate-pop-out">  {/* Animation added as child so parent elements scale isnt effected */}
-                                                <div className="absolute bg-primary w-full h-full z-10 brightness-[20%] md:brightness-100 opacity-90 md:opacity-30 md:hover:opacity-0 transition duration-300 ease-in-out"></div>
-                                                <img className="rounded-sm max-w-[600px] h-[337px]" alt="Image of feature project" src={items.imageSrc}></img>
-                                            </div>
-                                        </motion.div>
+          {/* Project information */}
+          <div class="p-5 sm:px-10 md:px-0 sm:py-14 md:py-10 backdrop-brightness-[0.3] md:backdrop-brightness-100 md:z-10 md:w-2/5 md:text-right">
+            <motion.div variants={rightCardVariants}>
+              <p class="text-primary">Personal Site</p>
+              <h3 class="mb-5 font-2xl font-semibold text-primary-text hover:text-primary transition duration-300 ease-in-out">Portfolio Website</h3>
 
-                                        {/* Text card */}
-                                        <motion.div className="flex flex-col items-start md:items-end z-20 max-w-[600px] md:w-2/5 mx-5 pb-5 absolute md:static">
-                                            <motion.div variants={rightCardVariants}>
-                                                <p className="text-primary">{items.tagline}</p>
-                                                <h3 className="mb-5 font-2xl font-semibold text-primary-text hover:text-primary transition duration-300 ease-in-out">{items.heading}</h3>
+              <p class="bg-none md:bg-quaternary md:pl-10 pr-5 sm:pt-5 pb-5 md:-ml-32 md:shadow-md md:rounded-sm text-gray-300">
+                My website showcasing projects I've created as well as a hub for marketing and contact.
+                Specifically, my first version of the portfolio site and how Ive adapted the project since then.
+              </p>
+            </motion.div>
 
-                                                <p className="bg-none md:bg-quaternary md:pl-10 pr-5 sm:pt-5 pb-5 md:-ml-32 shadow-md hover:shadow-lg rounded-sm text-gray-300">
-                                                    {items.text}
-                                                </p>
-                                            </motion.div>
-       
-                                            {/* Stack list */}
-                                            <motion.ul variants={listVariants} id="projectList" className="flex flex-row sm:pt-5">
-                                                <li>{items.list}</li>
-                                            </motion.ul>
-                                        </motion.div>
-                                    
-                                </div>
-                            </Link>
-                        </motion.div>
-                    )
-                )}
-                    
-                {items.map((items, index) => 
-                    index == 1 && (
-                        //Project Two
-                        <motion.div variants={containerVariants} initial="hidden" whileInView="visible" viewport={{ once: true }}> {/* Motions own container as not compatible with Link */}
-                            <Link to={items.link}> {/* Motions own container as not compatible with Link */}
-                                <div className="mt-8 lg:mt-24 md:space-x-10 flex md:flex-row items-center"> {/* Objects mapped ID used so all mapped elements have scroll listener applied */}
-                                    {/* Text card */}
-                                    <motion.div variants={textVariants} className="flex flex-col items-start z-20 max-w-[600px] md:w-2/5 mx-5 pb-5 absolute md:static">
-                                        <motion.div variants={leftCardVariants}>
-                                            <p className="text-primary">{items.tagline}</p>
-                                            <h3 className="mb-5 font-2xl font-semibold text-primary-text hover:text-primary transition duration-300 ease-in-out">{items.heading}</h3>
+            {/* Stack list */}
+            <motion.ul
+              variants={listVariants}
+              class="flex flex-col flex-wrap gap-y-2 sm:flex-row space-y-2.5 sm:space-y-0 sm:space-x-5 md:justify-end sm:pt-5 md:-ml-32"
+            >
+              <li>Handlebars/React</li>
+              <li>ExpressJS</li>
+              <li>NodeJS</li>
+              <li>CSS/Tailwind</li>
+            </motion.ul>
+          </div>
+        </motion.div>
+    );
+  };
 
-                                            <p className="bg-none md:bg-quaternary text-left md:pl-5 pr-10 sm:pt-5 pb-5 md:-mr-32 shadow-md hover:shadow-lg rounded-sm text-gray-300">
-                                                {items.text}
-                                            </p>
-                                        </motion.div>
+  // Left aligned card
+  function AlignedLeftCard() {
+    return (
+      <motion.div 
+        variants={containerVariants}
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true }}
+        onClick={() => navigate("/projects/one-messaging")}
+        class="relative bg-[url('../public/Images/one-messaging-thumbnail.webp')] md:bg-none bg-fit bg-no-repeat bg-center shadow-[inset_0_2000px_0_0_rgba(76,107,193,0.3)] md:shadow-none md:flex md:flex-row md:space-x-5 hover:cursor-pointer"
+      >
+        {/* Project information */}
+        <div class="p-5 sm:px-10 md:px-0 sm:py-14 md:py-10 backdrop-brightness-[0.3] md:backdrop-brightness-100 md:z-20 md:w-2/5">
+          <motion.div variants={leftCardVariants}>
+            <p class="text-primary">Messaging Project</p>
+            <h3 class="mb-5 font-2xl font-semibold text-primary-text hover:text-primary transition duration-300 ease-in-out">One messaging project</h3>
 
-                                        {/* Stack list */}
-                                        <motion.ul variants={listVariants} id="projectList" className="flex flex-row sm:pt-5">
-                                            <li>{items.list}</li>
-                                        </motion.ul>
-                                    </motion.div>
+            <p class="bg-none md:bg-quaternary md:px-5 sm:pt-5 pb-5 md:-mr-32 md:shadow-md md:rounded-sm text-gray-300">
+              A realtime messaging app I made in my second year of University.
+              This was project portion of my research into the advancement web communication systems.
+            </p>
+          </motion.div>
 
-                                    {/* Image */}
-                                    <motion.div variants={imageVariants} className="relative overflow-hidden"> {/* Container for overlaying child elements */}
-                                        <div className="hover:animate-pop-out">  {/* Animation added as child so parent elements scale isnt effected */}
-                                            <div className="absolute bg-primary w-full h-full z-10 brightness-[20%] md:brightness-100 opacity-90 md:opacity-30 md:hover:opacity-0 transition duration-300 ease-in-out"></div>
-                                            <img className="rounded-sm max-w-[600px]" alt="Image of feature project" src={items.imageSrc}></img>
-                                        </div>
-                                    </motion.div>
-                                </div>
-                            </Link> 
-                        </motion.div>
-                    )
-                )}
-            </>  
-        </>
-    )
-}
+          {/* Stack list */}
+          <motion.ul
+            variants={listVariants}
+            class="flex flex-col flex-wrap gap-y-2 sm:flex-row space-y-2.5 sm:space-y-0 sm:space-x-5 sm:pt-5 md:-mr-32"
+          >
+            <li>Handlebars</li>
+            <li>ExpressJS</li>
+            <li>NodeJS</li>
+            <li>SocketIO</li>
+            <li>SQLite</li>
+          </motion.ul>
+        </div>
+
+        {/* Image */}
+        <motion.div
+        variants={imageVariants}
+        class="hidden md:block md:w-3/5 md:relative md:overflow-hidden"
+        >
+          <div class="hover:scale-105 transition duration-500 ease-in-out">
+            {/* Image overlay */}
+            <div class="absolute bg-primary w-full h-full z-10 opacity-30 md:hover:opacity-0 transition duration-500 ease-in-out"></div>
+
+            <img
+              src="../Images/one-messaging-thumbnail.webp"
+              alt="Featured project"
+              class="rounded max-w-[600px]"
+            ></img>
+          </div>
+        </motion.div>
+      </motion.div>
+    );
+  };
+  
+  return (
+    <>
+      {/* Heading */}
+      <div class="space-y-3 mx-auto">
+        <h2 class="font-semibold text-4xl text-center">Featured Projects</h2>
+        <div class="w-48 h-1 bg-primary mx-auto"></div>
+      </div>
+
+      {/* Project cards */}
+      <div class="mt-12 space-y-10 lg:space-y-24">
+        <AlignedRightCard />
+        <AlignedLeftCard />
+      </div>
+    </>
+  );
+};
