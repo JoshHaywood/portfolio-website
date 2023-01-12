924de4d26aad7ef5f35af783fbc7fae1b5227b0c
JOSH Haywood <jh248828@falmouth.ac.uk>
2023-01-12 20:21:32 +0000

    Update header.js
    - Improved animation, header hides when scrolling down and appears when scrolling up

--- src/components/header.js
+++ src/components/header.js
@@ -3,55 +3,78 @@ import { motion } from "framer-motion";
 import { useNavigate } from "react-router-dom";
 import { Squash as Hamburger } from "hamburger-react";
 
-import NavLinks from './navLinks';
+import NavLinks from "./navLinks";
 
 export default function Header() {
   const navigate = useNavigate();
   const [src, setSrc] = useState('../Images/logo.png');
   const [sidebar, setSidebar] = useState(false);
   const [scrolled, setScrolled] = useState(false);
+  const [lastScrollY, setLastScrollY] = useState(0);
 
   const isMobile = window.innerWidth < 640;
   const isntDesktop = window.innerWidth < 768;
 
-  useEffect(() => {
-    const handleScroll = () => {
-      // If user scrolls down, set scrolled to true
-      if (window.scrollY > 0) {
-        setScrolled(true);
-        // Else set scrolled to false
-      } else {
-        setScrolled(false);
-      }
-    };
+  const [backgroundColor, setBackgroundColor] = useState('transparent');
 
-    window.addEventListener('scroll', handleScroll);
-  }, []);
+  useEffect(() => {
+      //Scroll handler
+      const handleScroll = () => {
+        const currentScrollY = window.scrollY; //Current scroll position
+        //If user scrolls down
+        if (currentScrollY > lastScrollY) {
+          setScrolled(true);
+
+          //If user is not at the top of the page
+          if (currentScrollY > 0) {
+            setBackgroundColor('#15171a');
+          }
+          //Else user scrolls up
+        } else {
+          setScrolled(false);
+
+          //If at user is at the top of the page
+          if (currentScrollY === 0) {
+            setBackgroundColor('transparent');
+          }
+        }
+        setLastScrollY(currentScrollY); //Set last scroll position to current
+      };
+      
+      window.addEventListener('scroll', handleScroll);
+      return () => window.removeEventListener('scroll', handleScroll);
+    }, [lastScrollY]);
+  
+
+useEffect(() => {
+  if (window.scrollY === 0) {
+    setScrolled(false);
+  }
+}, [window.scrollY]);
 
   return (
     <>
       {/* Overlay */}
-      <div class={`${
-        sidebar
-          ? `z-50 h-screen w-screen fixed bg-black opacity-60 backdrop-blur`
-          : `hidden`
-      }`}
+      <div
+        class={`${
+          sidebar
+            ? `z-50 h-screen w-screen fixed bg-black opacity-60 backdrop-blur`
+            : `hidden`
+        }`}
       ></div>
 
       <motion.nav
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
+        initial={{ y: '0%', backgroundColor: backgroundColor }}
+        animate={{ y: scrolled ? '-100%' : '0%', backgroundColor: backgroundColor }}
         transition={{ duration: 0.5, delay: 0.3 }}
-        class={`${
-          sidebar || scrolled ? `bg-tertiary` : `bg-none`
-        } w-full fixed top-0 py-1 sm:py-3 px-2.5 sm:px-5 flex justify-between items-center z-50`}
+        class="w-full fixed top-0 py-1 sm:py-3 px-2.5 lg:px-10 flex justify-between items-center z-50 backdrop-blur"
       >
         {/* Logo */}
         <img
           src={src}
-          onMouseEnter={() => setSrc('../Images/logo-hover.png')}
-          onMouseLeave={() => setSrc('../Images/logo.png')}
-          onClick={() => navigate('/')}
+          onMouseEnter={() => setSrc("../Images/logo-hover.png")}
+          onMouseLeave={() => setSrc("../Images/logo.png")}
+          onClick={() => navigate("/")}
           alt="Logo"
           class="w-[200px] sm:w-[250px] h-auto px-2 pr-0 sm:pr-2 backdrop-blur hover:cursor-pointer"
         ></img>
@@ -68,12 +91,9 @@ export default function Header() {
               toggled={sidebar}
             />
           )}
-          
+
           {/* Navlinks */}
-          <NavLinks
-            sidebar={sidebar}
-            setSidebar={setSidebar}
-          />
+          <NavLinks sidebar={sidebar} setSidebar={setSidebar} />
         </div>
       </motion.nav>
     </>
