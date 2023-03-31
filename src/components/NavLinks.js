50fefecd4a53f2fd03ae9fbec36c9f0d1ae308bd
Josh Haywood <Joshhaywood025@gmail.com>
2023-03-31 19:08:32 +0100

    Update CV route.

--- src/components/NavLinks.js
+++ src/components/NavLinks.js
@@ -115,7 +115,7 @@ export default function NavLinks(props) {
 
           <div class="md:mr-3" >
             <Button
-              href={`${process.env.PUBLIC_URL}/documents/cv.pdf`}
+              href={`${process.env.PUBLIC_URL}/documents/josh-haywood-cv.pdf`}
               target="_blank"
               sx={{
                 textTransform: "none",
