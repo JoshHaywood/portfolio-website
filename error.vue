ba268b4674b481286d02cf59b202ae3146cf3c1f
JoshHaywood <Joshhaywood025@gmail.com>
2023-09-15 21:33:48 +0100

    Fix missing error components
    - Add header and footer to error page

--- error.vue
+++ error.vue
@@ -1,7 +1,13 @@
 <template>
-  <StatusMessage
-    :heading="'This page does not exist'"
-    :message="'The page you were looking for does not exist or has been removed.'"
-    :redirect="-1"
-  />
+  <div>
+    <MainHeader />
+
+    <StatusMessage
+      :heading="'This page does not exist'"
+      :message="'The page you were looking for does not exist or has been removed.'"
+      :redirect="-1"
+    />
+
+    <MainFooter />
+  </div>
 </template>
