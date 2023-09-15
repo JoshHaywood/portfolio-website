e0f506e94aa07d2509c78263a3eed3563699ad4a
JoshHaywood <Joshhaywood025@gmail.com>
2023-09-15 21:57:31 +0100

    Add header and navlinks components
    - Convert header and navlinks
    - Replace framer motion transitions for vue transitions

--- components/MainHeader.vue
+++ components/MainHeader.vue
@@ -1,1 +1,106 @@
-<template>Header</template>
+<template>
+  <div>
+    <div v-show="sidebar" class="fixed top-0 z-40 h-screen w-screen bg-black opacity-70" @click="sidebar = false"></div>
+
+    <Transition name="slide">
+      <nav
+        v-show="showHeader"
+        :class="sidebar ? 'bg-tertiary' : 'bg-transparent'"
+        class="fixed top-0 z-40 flex w-full items-center justify-between p-3 backdrop-blur transition duration-300 lg:px-10"
+      >
+        <img
+          :src="src"
+          alt="Logo"
+          class="h-auto w-[200px] px-2 pr-0 hover:cursor-pointer sm:w-[250px] sm:pr-2"
+          @mouseenter="src = '../images/logo-hover.png'"
+          @mouseleave="src = '../images/logo.png'"
+        />
+
+        <button class="absolute right-3 block h-5 w-5 md:hidden" @click="sidebar = !sidebar">
+          <div
+            :class="sidebar ? 'absolute top-1/2 -translate-y-1/2 rotate-45' : 'absolute top-0'"
+            class="h-0.5 w-6 bg-white transition-transform"
+          ></div>
+          <div
+            :class="sidebar ? 'absolute top-1/2 -translate-y-1/2 opacity-0' : 'absolute top-1/2 -translate-y-1/2'"
+            class="h-0.5 w-6 bg-white transition-opacity"
+          ></div>
+          <div
+            :class="sidebar ? 'absolute top-1/2 -translate-y-1/2 -rotate-45' : 'absolute bottom-0'"
+            class="h-0.5 w-6 bg-white transition-transform"
+          ></div>
+        </button>
+
+        <NavLinks :sidebar="sidebar" />
+      </nav>
+    </Transition>
+  </div>
+</template>
+
+<script setup lang="ts">
+const src = ref<string>('../images/logo.png');
+
+const sidebar = ref<boolean>(false);
+const showHeader = ref<boolean>(true);
+
+let lastScrollY = 0;
+
+const handleScroll = () => {
+  const newY = window.scrollY;
+
+  if (sidebar.value) {
+    showHeader.value = true;
+    return;
+  }
+
+  if (newY < lastScrollY) {
+    showHeader.value = true;
+  } else if (newY > lastScrollY) {
+    showHeader.value = false;
+  }
+
+  lastScrollY = newY;
+};
+
+const updateSidebarState = () => {
+  if (window.innerWidth > 640) {
+    sidebar.value = false;
+  }
+};
+
+watch(sidebar, (newValue) => {
+  document.body.style.overflow = newValue ? 'hidden' : 'auto';
+
+  if (newValue) {
+    nextTick(() => {
+      showHeader.value = true;
+    });
+  }
+});
+
+onMounted(() => {
+  window.addEventListener('resize', updateSidebarState);
+  window.addEventListener('scroll', handleScroll);
+  updateSidebarState();
+});
+
+onUnmounted(() => {
+  window.removeEventListener('resize', updateSidebarState);
+  window.removeEventListener('scroll', handleScroll);
+});
+</script>
+
+<style scoped>
+.slide-enter-active {
+  transform: translateY(0);
+}
+
+.slide-leave-active {
+  transform: translateY(-100%);
+}
+
+.slide-enter-from,
+.slide-leave-to {
+  transform: translateY(-100%);
+}
+</style>
---
+++ components/NavLinks.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="relative">
+    <div class="hidden items-center space-x-2.5 md:flex">
+      <a href="/documents/josh-haywood-cv.pdf" target="_blank">
+        <button
+          class="mx-auto rounded border-2 border-primary bg-primary/30 p-2 text-sm font-semibold text-white transition-colors duration-300 hover:bg-transparent"
+        >
+          View my CV
+        </button>
+      </a>
+
+      <ul class="flex space-x-5">
+        <li
+          v-for="link in links"
+          :key="link.id"
+          class="ml-2.5 text-lg font-semibold text-white transition duration-300 hover:cursor-pointer hover:text-primary"
+        >
+          {{ link.name }}
+        </li>
+      </ul>
+    </div>
+
+    <Transition name="slide">
+      <div
+        v-if="sidebar"
+        class="fixed right-0 mt-6 flex h-screen w-[280px] flex-col items-center bg-tertiary pt-40 transition duration-300 md:hidden"
+      >
+        <ul class="flex flex-col space-y-10">
+          <li
+            v-for="link in links"
+            :key="link.id"
+            class="ml-2.5 text-lg font-semibold text-white transition duration-300 hover:cursor-pointer hover:text-primary"
+          >
+            {{ link.name }}
+          </li>
+
+          <a href="/documents/josh-haywood-cv.pdf" target="_blank">
+            <button
+              class="mx-auto rounded border-2 border-primary bg-primary/30 p-2 text-sm font-semibold text-white transition-colors duration-300 hover:bg-transparent"
+            >
+              View my CV
+            </button>
+          </a>
+        </ul>
+      </div>
+    </Transition>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  sidebar: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+const links = [
+  { id: 'about', name: 'About' },
+  { id: 'skills', name: 'Skills' },
+  { id: 'projects', name: 'Projects' },
+  { id: 'contact', name: 'Contact' },
+];
+</script>
+
+<style scoped>
+.slide-enter-active {
+  transform: translateX(0);
+}
+
+.slide-leave-active {
+  transform: translateX(100%);
+}
+
+.slide-enter-from,
+.slide-leave-to {
+  transform: translateX(100%);
+}
+</style>
