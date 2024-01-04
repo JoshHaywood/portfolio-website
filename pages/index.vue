3459a20d3f75d9c3b4e83884171116a0c4f9170f
Josh Haywood <Joshhaywood025@gmail.com>
2024-01-04 22:14:39 +0000

    Create hero background
    - Create new particle file for hero background
    - Replace index placeholders with hero background

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,3 @@
 <template>
-  <div>
-    <div class="mt-40">
-      Count: {{ counter.count }}
-      <button class="bg-primary" @click="counter.increment">Increment</button>
-    </div>
-    <div class="mt-[1000px]">
-      Count: {{ counter.count }}
-      <button class="bg-primary" @click="counter.increment">Increment</button>
-    </div>
-  </div>
+  <BackgroundParticles />
 </template>
-
-<script setup lang="ts">
-import { useCounterStore } from '@/stores/counter';
-
-const counter = useCounterStore();
-</script>
