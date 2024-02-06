3b3961f0adf11ea3349ddbb0abc18bee604cafaa
Josh Haywood <Joshhaywood025@gmail.com>
2024-02-06 22:05:47 +0000

    Create skill set section
    - Create skill set component
    - Move heading spacing into heading component directly

--- components/index/AboutMe.vue
+++ components/index/AboutMe.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="space-y-12">
+  <div>
     <!-- Heading -->
     <SectionHeading heading="About Me" />
 
--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,7 @@
       class="max-w-[1100px] mt-20 sm:mt-28 mb-36 md:mb-56 lg:mb-64 mx-auto px-5 xl:px-0 space-y-32 md:space-y-48 xl:space-y-56"
     >
       <AboutMe />
+      <SkillSet />
     </div>
   </div>
 </template>
