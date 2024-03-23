5f4bb989a50123484a239f95b92f2780349f1237
Josh Haywood <Joshhaywood025@gmail.com>
2024-03-23 00:35:32 +0000

    Create project cards
    - Create project cards component

--- components/index/projects/ProjectContainer.vue
+++ components/index/projects/ProjectContainer.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
     <SectionHeading heading="Featured Projects" />
-    <FeaturedProjects />
+
+    <div class="mt-12 space-y-10 lg:space-y-24">
+      <FeaturedProjects />
+      <ProjectCards />
+    </div>
   </div>
 </template>
