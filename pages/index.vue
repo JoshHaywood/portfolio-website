76e8839a36c2a0a241d456948bd2773f152b940b
Josh Haywood <Joshhaywood025@gmail.com>
2024-05-15 22:19:46 +0100

    Animate index sections
    - Add motion animations to major index sections, excluding projects

--- pages/index.vue
+++ pages/index.vue
@@ -5,10 +5,27 @@
     <div
       class="max-w-[1100px] mt-20 sm:mt-28 mb-36 md:mb-56 lg:mb-64 mx-auto px-5 xl:px-0 space-y-28 md:space-y-48 xl:space-y-56"
     >
-      <AboutMe />
-      <SkillSet />
-      <ProjectContainer />
-      <ContactForm />
+      <AboutMe v-motion :initial="initial()" :visible-once="animation()" />
+      <SkillSet v-motion :initial="initial()" :visible-once="animation()" />
+      <ProjectContainer v-motion :initial="initial()" :visible-once="animation()" />
+      <ContactForm v-motion :initial="initial()" :visible-once="animation()" />
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+const initial = () => ({
+  opacity: 0,
+  y: 20,
+});
+
+const animation = () => ({
+  opacity: 1,
+  y: 0,
+  transition: {
+    delay: 200,
+    type: 'keyframes',
+    ease: 'easeInOut',
+  },
+});
+</script>
